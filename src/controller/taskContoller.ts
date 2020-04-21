5b558c1b21997fd6cdbf4cf520aac11e32f8a011
Jeff Liu <jeff@Jeffs-MBP.fios-router.home>
2020-04-21 18:51:59 -0400

    adding all CRUD api functions

--- src/controller/taskContoller.ts
+++ src/controller/taskContoller.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from 'express';
 import { MongoHelper } from '../mongoHelper';
+const ObjectId = require('mongodb').ObjectId;
+import { Validator } from 'node-input-validator';
+import { v4 as uuid } from 'uuid';
 
 class TaskController {
   getCollection = () => {
@@ -10,7 +13,6 @@ class TaskController {
     try {
       const taskList: any = this.getCollection();
       const data = await taskList.find({}).toArray();
-      console.log(data);
       res.status(200).json({
         status: 'success',
         data,
@@ -21,6 +23,220 @@ class TaskController {
         message: e.message,
       });
     }
+  };
+
+  getTaskById = async (req: Request, res: Response) => {
+    try {
+      const taskCollection: any = this.getCollection();
+      const thisTask = await taskCollection.findOne(ObjectId(req.params.id));
+      if (thisTask === null) throw new Error('400 No task found with this Id');
+
+      res.status(200).json({
+        status: 'success',
+        data: {
+          thisTask,
+        },
+      });
+    } catch (e) {
+      this.errorHandler(res, e);
+    }
+  };
+
+  createNewTask = async (req: Request, res: Response) => {
+    try {
+      const v = new Validator(req.body, {
+        title: 'required|string',
+        description: 'required|string',
+        hoursEstimated: 'required|integer',
+        completed: 'required|in:true,false',
+        comments: 'required|array',
+      });
+      const matched = await v.check();
+      if (!matched) throw new Error('400 creating task info validating failed');
+      const obj: Array<Object> = req.body.comments;
+      for (var i = 0; i < obj.length; i++) {
+        const eachCommentValidate: any = new Validator(obj[i], {
+          name: 'required|string',
+          comment: 'required|string',
+        });
+        const eachCommentValidateResult = await eachCommentValidate.check();
+        if (!eachCommentValidateResult) {
+          throw new Error('400 creating task comment info validating failed');
+        }
+      }
+
+      var commentsEdit = req.body.comments.map(function (el) {
+        var o = Object.assign({}, el);
+        const id: string = uuid();
+        o.id = id;
+        return o;
+      });
+      let task = {
+        title: req.body.title,
+        description: req.body.description,
+        hoursEstimated: req.body.hoursEstimated,
+        completed: req.body.completed,
+        comments: commentsEdit,
+      };
+      const taskCollection: any = this.getCollection();
+      await taskCollection.insertOne(task);
+      res.status(200).json({
+        status: 'success',
+        data: {
+          task,
+        },
+      });
+    } catch (e) {
+      res.status(500).json({
+        status: 'fail',
+        message: e.message,
+      });
+    }
+  };
+
+  updateTask = async (req: Request, res: Response) => {
+    try {
+      const v: Validator = new Validator(req.body, {
+        title: 'required|string',
+        description: 'required|string',
+        hoursEstimated: 'required|integer',
+        completed: 'required|in:true,false',
+      });
+      const matched = await v.check();
+      if (!matched) throw new Error('400 Updating task info validating failed');
+      console.log('hi');
+      let updateInfo = {
+        title: req.body.title,
+        description: req.body.description,
+        hoursEstimated: req.body.hoursEstimated,
+        completed: req.body.completed,
+      };
+      const taskCollection: any = this.getCollection();
+      await taskCollection.findOneAndUpdate(
+        { _id: ObjectId(req.params.id) },
+        { $set: updateInfo }
+      );
+      const newTask = await taskCollection.findOne(ObjectId(req.params.id));
+      res.status(200).json({
+        status: 'success',
+        newTask,
+      });
+    } catch (e) {
+      console.log(e.message);
+      this.errorHandler(res, e);
+    }
+  };
+
+  patchUpdateTaskById = async (req: Request, res: Response) => {
+    try {
+      const result = {};
+      Object.entries(req.body).forEach(([key, value]) => {
+        if (key === 'title') {
+          if (typeof value === 'string' || value instanceof String) {
+            Object.assign(result, { title: value });
+          } else {
+            throw new Error('400 patching task title validating failed');
+          }
+        } else if (key === 'description') {
+          if (typeof value === 'string' || value instanceof String) {
+            Object.assign(result, { description: value });
+          } else {
+            throw new Error('400 patching task description validating failed');
+          }
+        } else if (key === 'hoursEstimated') {
+          if (typeof value === 'number') {
+            Object.assign(result, { hoursEstimated: value });
+          } else {
+            throw new Error('400 patching task description validating failed');
+          }
+        } else if (key === 'completed') {
+          if (typeof value === 'boolean') {
+            Object.assign(result, { complted: value });
+          } else {
+            throw new Error('400 patching task completed validating failed');
+          }
+        }
+      });
+
+      const taskCollection: any = this.getCollection();
+      await taskCollection.findOneAndUpdate(
+        { _id: ObjectId(req.params.id) },
+        { $set: result }
+      );
+      const newTask = await taskCollection.findOne(ObjectId(req.params.id));
+      res.status(200).json({
+        status: 'success',
+        newTask,
+      });
+    } catch (e) {
+      this.errorHandler(res, e);
+    }
+  };
+
+  addCommentById = async (req: Request, res: Response) => {
+    try {
+      const v = new Validator(req.body, {
+        name: 'required|string',
+        comment: 'required|string',
+      });
+      const matched = await v.check();
+      if (!matched)
+        throw new Error('400 not enough information for adding comment');
+      const taskCollection: any = this.getCollection();
+      const thisTask = await taskCollection.findOne(ObjectId(req.params.id));
+      if (thisTask === null) throw new Error('400 No task found with this Id');
+      await taskCollection.updateOne(
+        { _id: ObjectId(req.params.id) },
+        {
+          $push: {
+            comments: {
+              id: uuid(),
+              name: req.body.name,
+              comment: req.body.comment,
+            },
+          },
+        }
+      );
+      const newTask = await taskCollection.findOne(ObjectId(req.params.id));
+      res.status(200).json({
+        status: 'success',
+        newTask,
+      });
+    } catch (e) {
+      this.errorHandler(res, e);
+    }
+  };
+
+  deleteCommentById = async (req: Request, res: Response) => {
+    try {
+      const taskCollection: any = this.getCollection();
+      const task = await taskCollection.findOne(ObjectId(req.params.taskId));
+      if (task === null) throw new Error('400 No task found with this Id');
+      const comments = task.comments;
+      if (!comments.some((e) => e.id === req.params.commentId)) {
+        throw new Error('400 No comment found with this Id');
+      }
+
+      await taskCollection.updateOne(
+        { _id: ObjectId(req.params.taskId) },
+        { $pull: { comments: { id: req.params.commentId } } },
+        false,
+        true
+      );
+      res.status(200).json({
+        status: 'success',
+        message: 'deleted comment successfully',
+      });
+    } catch (e) {
+      this.errorHandler(res, e);
+    }
+  };
+
+  errorHandler = (res: Response, e: Error) => {
+    res.status(parseInt(e.message.substring(0, 3))).json({
+      status: 'fail',
+      error: e.message.substring(4),
+    });
   };
 }
 
--- src/routes/taskRoutes.ts
+++ src/routes/taskRoutes.ts
@@ -1,8 +1,61 @@
 import TaskController from '../controller/taskContoller';
+import { Request, Response, NextFunction } from 'express';
+import * as express from 'express';
+
+import * as util from 'util';
+import * as morgan from 'morgan';
 
 export class TaskRoutes {
   public controller: TaskController = new TaskController();
-  public routes(app): void {
+  public map: Map<string, number> = new Map();
+
+  public routes(app: express.Application): void {
+    app.use(morgan('dev'));
+    app.use(this.LoggerMiddleware);
+    app.use(this.countRequest);
+
+    app.use('*', (req: Request, res: Response) => {
+      res.status(404).json({ error: 'Not found' });
+    });
+
     app.route('/api/tasks/').get(this.controller.getAllTasks);
+    app
+      .route('/api/tasks/:id')
+      .get(this.idChecker, this.controller.getTaskById);
+    app.route('/api/tasks/').post(this.controller.createNewTask);
+    app.route('/api/tasks/:id').put(this.idChecker, this.controller.updateTask);
+    app
+      .route('/api/tasks/:id')
+      .patch(this.idChecker, this.controller.patchUpdateTaskById);
+    app
+      .route('/api/tasks/:id/comments')
+      .post(this.idChecker, this.controller.addCommentById);
+    app
+      .route('/api/tasks/:taskId/:commentId')
+      .delete(this.idChecker, this.controller.deleteCommentById);
   }
+  public idChecker(req: Request, res: Response, next: NextFunction) {
+    if (
+      (req.params.id && !req.params.id.match(/^[0-9a-fA-F]{24}$/)) ||
+      (req.params.taskId && !req.params.taskId.match(/^[0-9a-fA-F]{24}$/))
+    ) {
+      return res
+        .status(400)
+        .json({ status: 'fail', error: 'It should be valid obejct id' });
+    }
+    next();
+  }
+  public LoggerMiddleware(req: Request, res: Response, next: NextFunction) {
+    console.log(`📑  Request.body \n${util.inspect(req.body, false, null)}`);
+    next();
+  }
+  public countRequest = (req: Request, res: Response, next: NextFunction) => {
+    if (this.map.has(req.url)) {
+      this.map.set(req.url, this.map.get(req.url) + 1);
+    } else {
+      this.map.set(req.url, 1);
+    }
+    console.log('#️⃣  Number of requests = ' + this.map.get(req.url));
+    next();
+  };
 }
